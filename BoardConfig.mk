f914310522d187d4fc04ca68c0619903b65e824e
Erik Drozina <erikdro41@gmail.com>
2022-01-03 00:46:00 +0100

    tucana: Add some flags to solve build errors
    Signed-off-by: Erik Drozina <erikdro41@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,10 @@ include device/xiaomi/sm6150-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/xiaomi/tucana
 
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Audio
 TARGET_PROVIDES_AUDIO_EXTNS := true
 
